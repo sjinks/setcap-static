fc34042e8a9db6cf52db8e88caf64057f4282853
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2021-05-30 17:07:59 +0300

    Add workflows

--- setcap.c
+++ setcap.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 {
     if (argc != 3) {
         fprintf(stderr, "Usage: setcap capabilities filename\n");
-        return EXIT_FAILURE;
+        return argc == 1 ? EXIT_SUCCESS : EXIT_FAILURE;
     }
 
     cap_t my_caps = cap_get_proc();
